cd32c1c8386a1705ebc8741c7891e6dbcc7808ff
Jakaria Hussain <40713469+jakariahussain@users.noreply.github.com>
2022-09-26 13:50:49 +0600

    done
    done addvance

---
+++ inc/footer.php
@@ -0,0 +1,178 @@
+<!-- footer-area-start -->
+<footer>
+    <div class="footer-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-6 col-md-4 col-lg-4 col-xl-4">
+                    <div class="footer-wrapp">
+                        <div class="footer-about text-center">
+                            <aside>
+                                <div class="footer-logo">
+                                    <figure>
+                                        <img src="images/logo/logo.png" alt="">
+                                    </figure>
+                                </div>
+                            </aside>
+                            <article>
+                                <p>Advanced Career Ltd, was founded with the aspiration of some highly qualified..</p>
+                            </article>
+                        </div>
+                        <div class="mailSubscribe">
+                            <form action="">
+                                <div class='mail'>
+                                    <input type="email" placeholder="Enter Your E-mail" class="form-control">
+                                </div>
+                                <div class="mail-submit">
+                                    <button class="btn btn-danger custom-btn" type='submit'>Sign Up</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="socials-links mt-3">
+                        <ul class="d-flex">
+                            <li><a href="javascript:void(0)"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="javascript:void(0)"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="javascript:void(0)"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="javascript:void(0)"><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href="javascript:void(0)"><i class="fab fa-youtube"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-2 col-xl-2">
+                    <div class="footer-services">
+                        <h3>our services</h3>
+                        <ul>
+                            <li><a href="">Education Consultancy</a></li>
+                            <li><a href="">Training Program</a></li>
+                            <li><a href="">Recruitment</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                    <div class="footer-services pl-0">
+                        <h3>Our London Office</h3>
+                        <div class="footer-address d-flex align-items-center">
+                            <div class="single-icon">
+                                <figure>
+                                    <img src="images/icon/phone-call.png" alt="">
+                                </figure>
+                            </div>
+                            <div class="footer-location">
+                                <ul>
+                                    <li><a href="javascript:void(0)">Phone</a></li>
+                                    <li><a href="tel:02036337173">02036337173</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="footer-address d-flex align-items-center">
+                            <div class="single-icon">
+                                <figure>
+                                    <img src="images/icon/email.png" alt="">
+                                </figure>
+                            </div>
+                            <div class="footer-location">
+                                <ul>
+                                    <li><a href="mailto:info@advancedcareer.uk">info@advancedcareer.uk</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="footer-address d-flex align-items-center">
+                            <div class="single-icon">
+                                <figure>
+                                    <img class="w-42" src="images/icon/place.png" alt="">
+                                </figure>
+                            </div>
+                            <div class="footer-location">
+                                <ul class="location">
+                                    <li>143 Cannon St Rd Whitechapel London E1 2LX,</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-3 col-xl-3">
+                    <div class="footer-services pl-0">
+                        <h3>Our Sylhet Office</h3>
+                        <div class="footer-address d-flex align-items-center">
+                            <div class="single-icon">
+                                <figure>
+                                    <img src="images/icon/phone-call.png" alt="">
+                                </figure>
+                            </div>
+                            <div class="footer-location">
+                                <ul>
+                                    <li><a href="javascript:void(0)">Phone</a></li>
+                                    <li><a href="tel:01312774417">01312774417</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="footer-address d-flex align-items-center">
+                            <div class="single-icon">
+                                <figure>
+                                    <img src="images/icon/email.png" alt="">
+                                </figure>
+                            </div>
+                            <div class="footer-location">
+                                <ul>
+                                    <li><a href="mailto:info@advancedcareer.uk">info@advancedcareer.uk</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="footer-address d-flex align-items-center">
+                            <div class="single-icon">
+                                <figure>
+                                    <img class="w-42" src="images/icon/place.png" alt="">
+                                </figure>
+                            </div>
+                            <div class="footer-location">
+                                <ul class="location">
+                                    <li>39-40 West World Shopping City, Zindabazar, Sylhet</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- copyrights-area -->
+<section class="copyrights-area">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="copyrights-wrapp d-flex justify-content-between align-items-center">
+                    <div class="copyrights">
+                        <p>Copyright © <span id='CurrentYear'></span> <a href="javascript:void(0)">advance-career</a> . Developed By <a
+                                href="javascript:void(0)">Forever IT</a></p>
+                    </div>
+                    <div class="term-area">
+                        <ul class='d-flex'>
+                            <li><a href="privacy-Policy.php">Privacy Policy</a></li>
+                            <li><a href="terms-condition.php">Terms & condition</a></li>
+                            <li><a href="refound.php">Refund Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<!-- All Js Plugins-->
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/fontawesome-all.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<script src="js/jquery.meanmenu.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/app.js"></script>
+
+</body>
+
+</html>
---
+++ inc/header.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Advance Career</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/meanmenu.min.css">
+    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/default.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <!-- topbar-contact -->
+    <div class="header-top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="topbar-wrapp d-flex justify-content-between align-items-center">
+                        <div class="advance-career-address">
+                            <ul class="d-flex">
+                                <li><span><i class="far fa-envelope"></i></span><a href="appointment.php">Book An Apponitment</a>
+                                </li>
+                                <li><span><i class="fa fa-phone"></i></span><a href="requestCall.php">Request A Call Back</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="check-application">
+                            <span class="mr-3"><i class="fa fa-sign-in-alt"></i></span><a href="studentLogin.php">Check
+                                your status</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- header-part -->
+    <header>
+        <div class="header sticky">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="header-wrapp d-flex justify-content-between align-items-center">
+                            <div class="logo">
+                                <a href="index.php"><img class="w-100" src="images/logo/logo.png" alt="Logo"></a>
+                            </div>
+                            <div class="primary-menu" id="mobile-menu">
+                                <nav>
+                                    <ul class="">
+                                        <li><a href="javascript:void(0)">About Us</a>
+                                            <ul>
+                                                <li><a href="about.php">About Us</a></li>
+                                                <li><a href="career.php">Career</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="javascript:void(0)">Services</a>
+                                            <ul>
+                                                <li><a href="services.php">Services</a></li>
+                                                <li><a href="service-details.php">Free counselling</a></li>
+                                                <li><a href="service-details.php">Student Adimission</a></li>
+                                                <li><a href="service-details.php">Student Accommodation</a></li>
+                                                <li><a href="service-details.php">scholarship</a></li>
+                                                <li><a href="service-details.php">Partner University Services</a></li>
+                                                <li><a href="service-details.php">End-to-End Services</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="javascript:void(0)">Study in the Uk</a>
+                                            <ul>
+                                                <li><a href="">Find a Course</a></li>
+                                                <li><a href="">Find a University</a></li>
+                                                <li><a href="">Uk university scholarship</a></li>
+                                                <li><a href="">application Process</a></li>
+                                                <li><a href="">Latest Uk university Ranking</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="javascript:void(0)">Global Destination</a>
+                                            <ul>
+                                                <li><a href="destination.php">Study in Australia</a></li>
+                                                <li><a href="destination.php">Study in Canada</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="gallery.php">Gallery</a></li>
+                                        <li><a href="event.php">Event</a></li>
+                                        <li><a href="blog.php">Blogs</a></li>
+                                        <li><a href="javascript:void(0)">Training</a></li>
+                                        <li><a href="contact.php">Contact Us</a></li>
+                                        <li><a href="javascript:void(0)" class="btn btn-danger">Apply now</a>
+                                            <ul>
+                                                <li><a href="apply.php">Uk Office</a></li>
+                                                <li><a href="apply.php">Sylhet Office</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div class="mobile-menu"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
